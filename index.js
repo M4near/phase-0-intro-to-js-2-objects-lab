5b27e62e8961ca58cafa908d66dac583c91020c8
Michael Fournier <michaeljfournier5@gmail.com>
2022-02-25 10:44:47 -0500

    commit tree 5934f8a2685d4ed06fb1eac9328b251b81a0fd49

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee ={
+    name: "Michael",
+    streetAddress: "100 Tunnel",
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    const employee1 = {...obj};
+    employee1[key] = value;
+    return employee1;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj,key){
+    const employee2 = {...obj};
+    employee2[key] = undefined;
+    return employee2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key,value){
+    employee[key] = undefined;
+    return employee;
+}
